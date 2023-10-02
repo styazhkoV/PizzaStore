7f3527903884a38295fc1aa4f63b632d751e2f6e
styazhkoV <alexasndr.styazhkov@gmail.com>
2023-10-02 14:30:16 +0600

    add EntityFrameworkCore

---
+++ Pizza.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using PizzaStore.Models;
+
+
+namespace PizzaStore.Models
+{
+    class PizzaDb : DbContext
+{
+    public PizzaDb(DbContextOptions options) : base(options) { }
+    public DbSet<Pizza> Pizzas { get; set; } = null!;
+}
+    public class Pizza
+    {
+        public int Id {get;set;}
+        public string? Name {get; set;}
+        public string? Description {get; set;}
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using PizzaStore.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen ( c=> 
+{
+    c.SwaggerDoc("v1", new OpenApiInfo{
+        Title = "PizzaStore API",
+        Description = "ТЕКСТ",
+        Version = "V1"});
+    });
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-
+//app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
+app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
+app.MapPost("/pizza", async (PizzaDb db, Pizza pizza) =>
+{
+    await db.Pizzas.AddAsync(pizza);
+    await db.SaveChangesAsync();
+    return Results.Created($"/pizza/{pizza.Id}", pizza);
+});
 app.Run();
